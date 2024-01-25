73855d8bffea536bd80c016a521aa11d8cac2e6f
chema <josemanuel.carrasco.lopez@gmail.com>
2024-01-25 17:13:11 +0100

    ajuste ruta

--- 213-A1/test_validador.py
+++ 213-A1/test_validador.py
@@ -9,7 +9,7 @@ def test_validar_xml():
     """
         Prueba unitaria validar xml
     """
-    xml_doc_ini = validador.obtener_documento_parseado("./carta.xml")
-    xsd_doc_ini = validador.obtener_documento_parseado("./carta.xsd")
+    xml_doc_ini = validador.obtener_documento_parseado("213-A1/carta.xml")
+    xsd_doc_ini = validador.obtener_documento_parseado("213-A1/carta.xsd")
     result = validador.validar_xml(xml_doc_ini, xsd_doc_ini)
     assert result is True
