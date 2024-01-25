51f73861b5a6a1adaf82382af8216289eeecc43f
chema <josemanuel.carrasco.lopez@gmail.com>
2024-01-25 16:35:40 +0100

    Test validator.py

--- 213-A1/test_validator.py
+++ 213-A1/test_validator.py
@@ -1,19 +1,15 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=I1101
 """
     Módulo prueba unitaria code_token
 """
 import unittest
+import validador
 from unittest.mock import patch
 from io import StringIO
 import sys
 import os
 from lxml import etree
-from code_token import (
-    obtener_documento_parseado,
-    validar_xml,
-    obtener_valores_xml,
-    xml_a_json,
-)
-
 
 class TestCodeToken(unittest.TestCase):
     """
@@ -26,7 +22,7 @@ class TestCodeToken(unittest.TestCase):
         xml_doc = etree.fromstring("<root></root>")
         xsd_doc = etree.fromstring("<xs:schema></xs:schema>")
         with patch("builtins.print") as mock_print:
-            validar_xml(xml_doc, xsd_doc)
+            validador.validar_xml(xml_doc, xsd_doc)
             mock_print.assert_called_with(
                 "    El documento XML es válido según el esquema XSD."
             )
@@ -38,7 +34,7 @@ class TestCodeToken(unittest.TestCase):
         xml_doc = etree.fromstring("<root><element>value</element></root>")
         xpath = "//element"
         with patch("builtins.print") as mock_print:
-            obtener_valores_xml(xml_doc, xpath)
+            validador.obtener_valores_xml(xml_doc, xpath)
             mock_print.assert_called_with("    > element: value")
 
     def test_xml_a_json(self):
@@ -49,7 +45,7 @@ class TestCodeToken(unittest.TestCase):
         with patch("builtins.print") as mock_print, patch(
             "builtins.open", create=True
         ) as mock_open:
-            xml_a_json(xml_doc)
+            validador.xml_a_json(xml_doc)
             mock_print.assert_called_with("- Convirtiendo a JSON: ")
             mock_open.assert_called_with("salida.json", "w", encoding="utf-8")
             mock_open.return_value.__enter__.return_value.write.assert_called()
