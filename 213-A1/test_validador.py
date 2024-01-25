2b6f10d8e31e367f05e7dc925ca3efced7134054
chema <josemanuel.carrasco.lopez@gmail.com>
2024-01-25 17:01:10 +0100

    ajuste

--- 213-A1/test_validador.py
+++ 213-A1/test_validador.py
@@ -9,7 +9,7 @@ def test_validar_xml():
     """
         Prueba unitaria validar xml
     """
-    xml_doc_ini = validador.obtener_documento_parseado("carta.xml")
-    xsd_doc_ini = validador.obtener_documento_parseado("carta.xsd")
+    xml_doc_ini = validador.obtener_documento_parseado("./carta.xml")
+    xsd_doc_ini = validador.obtener_documento_parseado("./carta.xsd")
     result = validador.validar_xml(xml_doc_ini, xsd_doc_ini)
     assert result is True
